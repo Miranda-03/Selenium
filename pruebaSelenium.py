fa3f309b1bfaa9758a6d54134b3c6119b5c271e9
miranda-03 <martin.miranda.2004@gmail.com>
2022-08-16 01:42:06 -0300

    pruebas con selenium

--- pruebaSelenium.py
+++ pruebaSelenium.py
@@ -1,2 +1,19 @@
 from selenium import webdriver
-webdriver.Firefox()
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+
+desired_cap = {
+    'browser': 'Chrome',
+    'browser_version': '84.0 beta',
+    'os': 'OS X',
+    'os_version': 'Catalina',
+    'resolution': '1024x768',
+    'name': 'Bstack-[Python] Sample Test'
+}
+
+driver = webdriver.Remote(
+        command_executor='kiyXF1ubqhXmAhBsidzT',
+        desired_capabilities=desired_cap
+    )
+driver.get('https://selenium-tp.herokuapp.com/')
